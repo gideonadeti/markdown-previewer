ebb344b2f24789c3e1d6690690435347fe02247e
Gideon Adeti <gideonadeti0@gmail.com>
2024-06-22 08:48:52 +0000

    Work on main contents

--- src/App.js
+++ src/App.js
@@ -12,7 +12,33 @@ function App() {
 					</div>
 				</nav>
 			</header>
-			<main className="flex-grow-1"></main>
+			<main className="flex-grow-1">
+				<div className="container p-4 mt-3 mt-md-4 mt-lg-5">
+					<div className="row row-cols-1 row-cols-md-2 flex-md-nowrap justify-content-center gap-3">
+						<div
+							className="col border border-3 shadow-sm py-2 px-3 bg-white rounded"
+							style={{ minHeight: "70vh" }}
+						>
+							<h2 className="text-center">Markdown</h2>
+							<textarea
+								className="markdown rounded shadow-sm p-2"
+								style={{
+									width: "100%",
+									height: "88%",
+									overflow: "hidden",
+								}}
+							></textarea>
+						</div>
+						<div
+							className="col border border-3 shadow-sm py-2 px-3 bg-white rounded"
+							style={{ minHeight: "70vh" }}
+						>
+							<h2 className="text-center">Previewer</h2>
+							<div className="previewer"></div>
+						</div>
+					</div>
+				</div>
+			</main>
 			<footer className="container text-center fw-medium text-secondary mt-auto">
 				<p>
 					&copy; {new Date().getFullYear()} Markdown Previewer.
