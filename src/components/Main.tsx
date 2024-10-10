3672fa0249171a443fc3aa5a95a65fcbea62b5f7
gideonadeti <gideonadeti0@gmail.com>
2024-10-10 15:06:11 +0000

    Style markdown

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,10 @@
 export default function Main() {
   return (
     <main className="flex-grow-1 container-fluid mt-5">
-      <div className="container row mx-auto gap-5">
+      <div
+        className="container row mx-auto gap-5"
+        style={{ minHeight: "75vh" }}
+      >
         <Markdown />
         <Previewer />
       </div>
@@ -10,7 +13,15 @@ export default function Main() {
 }
 
 function Markdown() {
-  return <div className="col border rounded shadow-sm p-3"></div>;
+  return (
+    <div className="col border rounded shadow-sm p-3 d-flex flex-column align-items-center">
+      <h2>Markdown</h2>
+      <textarea
+        className="rounded shadow-sm p-3 form-control flex-grow-1"
+        style={{ resize: "none" }}
+      />
+    </div>
+  );
 }
 
 function Previewer() {
