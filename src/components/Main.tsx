e9d1921bf6a946c9007e61d9701fc8526aa7453c
gideonadeti <gideonadeti0@gmail.com>
2024-10-10 14:52:03 +0000

    Set up mardown and previewer

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,18 @@
 export default function Main() {
-  return <main className="flex-grow-1"></main>;
+  return (
+    <main className="flex-grow-1 container-fluid mt-5">
+      <div className="container row mx-auto gap-5">
+        <Markdown />
+        <Previewer />
+      </div>
+    </main>
+  );
+}
+
+function Markdown() {
+  return <div className="col border rounded shadow-sm p-3"></div>;
+}
+
+function Previewer() {
+  return <div className="col border rounded shadow-sm p-3"></div>;
 }
